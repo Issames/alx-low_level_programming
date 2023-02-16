f80d5b6a18f65343d9310d7a90a8152742403c09
Issames <lamchabragissame.112@gmail.com>
2023-02-16 04:53:41 -0800

    fix Belly coding style errot

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
- #include<stdio.h>
+#include<stdio.h>
  
- /**
- * main - First Entry point
- *
- * Description: prints "Programming is like building a multilingual puzzle"
- *
- * Return: Always 0
- */
-   int main(void)
-  {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-  }
+/**
+* main - First Entry point
+*
+* Description: prints "Programming is like building a multilingual puzzle"
+*
+* Return: Always 0
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
