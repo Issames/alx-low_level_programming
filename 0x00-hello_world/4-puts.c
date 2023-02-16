626a859926e893efa1ed54c744706e0035784514
Issames <lamchabragissame.112@gmail.com>
2023-02-16 04:44:54 -0800

    Test 01

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 /**
- * main - Entry point
+ * main - First Entry point
  *
  * Description: prints "Programming is like building a multilingual puzzle"
  *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 
-int main(void){
+   int main(void)
+  {
 	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
-}
+  }
