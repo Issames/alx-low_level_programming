c0e8c256046f96ba085e7038dcce0b4ae9c84d87
Issames <lamchabragissame.112@gmail.com>
2023-02-16 05:04:18 -0800

    Test05

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - Start Point
+ *
+ * Description : prints : "with proper grammar, but the outcome is a piece of art,"
+ *
+ * Return : always 0
+ *
+ */
+int main(void){
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
+
