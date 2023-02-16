b43783e479a822c5a8d7c832ac98952e8a22dbad
Issames <lamchabragissame.112@gmail.com>
2023-02-16 05:13:00 -0800

    T

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main - Start Point
+ * main - Entry  Point
  *
- * Description : prints : "with proper grammar, but the outcome is a piece of art,"
+ * Description: prints : "with proper grammar, but the outcome is a piece of art,"
  *
- * Return : always 0
+ * Return: Always 0
  *
  */
 int main(void)
