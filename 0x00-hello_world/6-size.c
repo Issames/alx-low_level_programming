ee05798ebca84473edc7d60ad2e107177af776a7
Issames <lamchabragissame.112@gmail.com>
2023-02-16 06:01:03 -0800

    P

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,8 +2,7 @@
 /*
  * main - print the size of various types
  *
- * Description: prints the size of various types on the computer it is compiled 
- * and run on
+ * Description: prints the size of various types on the computer it is compiled and run on
  *
  * Return: Always 0
  */
