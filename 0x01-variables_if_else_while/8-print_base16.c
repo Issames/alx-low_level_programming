eddd0b3a3a5b359cf66dd63dcca19cee3ba8c737
Issames <lamchabragissame.112@gmail.com>
2023-02-17 12:49:00 -0800

    hexa

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+/**
+ * main - print the letters of the alphabet
+ *
+ * Description: print the letters of the hexa mode
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+
+int i = 0;
+
+	while (i < 48)
+	{
+		if (i < 10)
+			putchar(i + '0');
+		else if (i > 41)
+			putchar(i - 10 + 'A');
+		i++;
+	}
+	putchar(10);
+
+	return (0);
+}
