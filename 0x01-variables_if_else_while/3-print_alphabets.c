c19e811609f35dc5019fe4a6840c40917d261110
Issames <lamchabragissame.112@gmail.com>
2023-02-17 11:48:41 -0800

    Lower & upper

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,19 +8,23 @@
  */
 int main(void)
 {
-int i ,j;
+int i, j;
+
 i = 97;
 j = 65;
+
 while (i < 123)
 {
 	putchar(i);
 	i++;
 }
+
 while (j < 91)
 {
 	putchar(j);
 	j++;
 }
+
 putchar(10);
 return (0);
 }
