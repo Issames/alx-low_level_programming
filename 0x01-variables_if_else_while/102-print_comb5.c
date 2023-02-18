8e0cb58e1dbdc5d373ef200c37b5ca0b95f5c283
Issames <lamchabragissame.112@gmail.com>
2023-02-18 05:03:52 -0800

    P

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,23 +10,40 @@
  */
 int main(void)
 {
-int i, j;
-for (i = 0; i <= 9; i++)
+int i, j, k, m;
+i = 48;
+while (i < 58)
 {
-	for (j = i; j <= 9; j++)
+	j = 48;
+	while (j < 58)
 	{
-		putchar('0' + i);
-		putchar('0' + j);
-		if (i == 9 && j == 9)
+		m = j + 1;
+		k = i;
+		while (k < 58)
 		{
-			putchar('\n');
+			m = j + 1;
+			k = i;
+			while (m < 58)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(32);
+				putchar(k);
+				putchar(m);
+				if (i < 57 || j < 56 || k < 57 || m < 57)
+				{
+					putchar(44);
+					putchar(32);
+				}
+				m++;
+			}
+			m = 48;
+			k++;
 		}
-		else
-		{
-			putchar(',');
-			putchar(' ');
-		}
+		j++;
 	}
+	i++;
 }
+putchar(10);
 return (0);
 }
