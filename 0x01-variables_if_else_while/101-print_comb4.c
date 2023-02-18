7639b1fa49cd84dfa2abd98ef97a70308a402de6
Issames <lamchabragissame.112@gmail.com>
2023-02-18 04:42:45 -0800

    P

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,23 +1,23 @@
 #include<stdio.h>
 /**
+ * main - print triple combos
  *
+ * Description: print triple digit combos
  *
- *
- *
- *
+ *Return: Always 0
  */
 int main(void)
 {
-int i,j,k;
-for (i = 0; i<=9; i++)
+int i, j, k;
+for (i = 0; i <= 9; i++)
 {
-	for (j = i + 1; j<=9; j++)
+	for (j = i + 1; j <= 9; j++)
 	{
-		for (k = j + 1; k<=9; k++)
+		for (k = j + 1; k <= 9; k++)
 		{
-			putchar('0'+ i);
+			putchar('0' + i);
 			putchar('0' + j);
-			putchar('0'+ k);
+			putchar('0' + k);
 			if (i == 7 && j == 8 && k == 9)
 			{
 				putchar('\n');
