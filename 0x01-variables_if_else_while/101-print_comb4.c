d7df7810d3889e589339c4cc51bb82f461e0e6a7
Issames <lamchabragissame.112@gmail.com>
2023-02-18 04:36:06 -0800

    P

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+int main(void)
+{
+int i,j,k;
+for (i = 0; i<=9; i++)
+{
+	for (j = i + 1; j<=9; j++)
+	{
+		for (k = j + 1; k<=9; k++)
+		{
+			putchar('0'+ i);
+			putchar('0' + j);
+			putchar('0'+ k);
+			if (i == 7 && i == 8 && i == 9)
+			{
+				putchar('\n');
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+}
+return (0);
+
+}
