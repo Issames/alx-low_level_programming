812a331f800379ce75b14e25abd022422dc2b231
Issames <lamchabragissame.112@gmail.com>
2023-02-21 12:22:21 -0800

    P

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -18,7 +18,7 @@ while (hours_tens < '3')
 		hours_max = '4';
 	}
 	hours_ones = '0';
-	while (hours_ones = hours_max)
+	while (hours_ones < hours_max)
 	{
 		minutes_tens = '0';
 		while (minutes_tens < '6')
