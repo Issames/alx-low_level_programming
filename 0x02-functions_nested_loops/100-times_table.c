3eb0b8ca1d51f824c826193d230a50e8bbaa1827
Issames <lamchabragissame.112@gmail.com>
2023-02-21 13:45:56 -0800

    P

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,7 +16,7 @@ if (n >= 0 && n < 15)
 {
 	for (row = 0; row <= n ; row++)
 	{
-		for (column = 0; column <= n; column ++)
+		for (column = 0; column <= n; column++)
 		{
 			product = row * column;
 			if (column == 0)
@@ -34,7 +34,15 @@ if (n >= 0 && n < 15)
 				_putchar(',');
 				_putchar(' ');
 				_putchar(product / 100 + '0');
-				_putchar((product/10) % 10 + '0');
+				_putchar((product / 10) % 10 + '0');
+				_putchar(product % 10 + '0');
+			}
+			else if (product > 99 && product < 1000)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(product / 100 + '0');
+				_putchar((product / 10) % 10 + '0');
 				_putchar(product % 10 + '0');
 			}
 		}
