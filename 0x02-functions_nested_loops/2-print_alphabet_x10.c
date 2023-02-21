17aa8a60b5eb5caf351ad64ace01dd83985ecf5a
Issames <lamchabragissame.112@gmail.com>
2023-02-21 10:14:28 -0800

    P

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+void print_alphabet_x10(void)
+{
+int r;
+char letter = 'a';
+for (r = 0; r < 10; r++)
+{
+	letter = 'a';
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}
+return (0);
+}
+
