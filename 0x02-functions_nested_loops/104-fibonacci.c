f2dcdf03720dd5ee1d52cae5de3f5deacb98619b
Issames <lamchabragissame.112@gmail.com>
2023-02-22 09:17:31 -0800

    P

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,17 +7,17 @@
  * Return: Always 0.
  */
 
-int main(void)
+int main()
 {
 int i;
-long long fib[100];
+unsigned long int fib[100];
 fib[0] = 1;
 fib[1] = 2;
-printf("%d, %d, ", fib[0], fib[1]);
-for ( i = 2; i < 98; i++)
+printf("%lu, %lu, ", fib[0], fib[1]);
+for (i = 2; i < 98; i++)
 {
 	fib[i] = fib[i-1] + fib[i-2];
-	printf("%d, ", fib[i]);
+	printf("%lu, ", fib[i]);
 }
 printf("\n");
 return (0);
