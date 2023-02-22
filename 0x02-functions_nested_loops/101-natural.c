5c32ca59ed9eaea125ca1c14346fea6119d0d9f2
Issames <lamchabragissame.112@gmail.com>
2023-02-22 07:53:04 -0800

    P

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,52 +1,20 @@
 #include "main.h"
 /**
- * print_times_table - prints the times table
- * @n: integer for which the times table will be printed
+ * main - list all natural numbers multiples of 3 & 5
  *
- * Description: prints the times table
- *
- * Return: void
+ * Return: Always 0.
  */
-
-void print_times_table(int n)
+int main(void)
 {
-int row, column, product;
-if (n >= 0 && n < 15)
+int num, sum;
+sum = 0;
+for (num = 0; num < 1024; num++)
 {
-	for (row = 0; row <= n; row++)
+	if (num % 3 == 0 || num % 5 == 0)
 	{
-		for (column = 0; column <= n; column++)
-		{
-			product = row * column;
-
-			if (column == 0)
-				_putchar('0');
-			else if (product < 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(product % 10 + '0');
-			}
-			else if (product >= 10 && product < 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar((product / 10) % 10 + '0');
-				_putchar(product % 10 + '0');
-			}
-			else if (product > 99 && product < 1000)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(product / 100 + '0');
-				_putchar((product / 10) % 10 + '0');
-				_putchar(product % 10 + '0');
-			}
-		}
-		_putchar('\n');
+		sum += num;
 	}
 }
+print("%d\n", sum);
+return (0);
 }
