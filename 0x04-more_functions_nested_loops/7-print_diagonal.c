cb55c2aab09e86ce9190f820546033c8fc6b6db7
Issames <lamchabragissame.112@gmail.com>
2023-02-23 08:47:00 -0800

    P

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print - diagnol - Draw a diagonal lines according parameter
+ * @n: number of times
+ *
+ * Return: Empty
+ */
+void print_diagonal(int n)
+{
+int x, y;
+if (n <= 0)
+{
+	_putchar('\n');
+}
+else
+{
+	for (x = 0; x < n; x++)
+	{
+		for (y = 0; y < x; y++)
+		{
+			_putchar(32);
+		}
+		_putchar(92);
+		_puctchar('\n');
+	}
+}
+}
+
