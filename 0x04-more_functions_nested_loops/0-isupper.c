d833003096ff0228042a1b17258332beceb6b1d5
Issames <lamchabragissame.112@gmail.com>
2023-02-23 06:47:11 -0800

    P

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - check for uppercase
+ * @n: character to checked for case
+ *
+ * Return: 1 if uppercase and 0 if lowercase
+ *
+ */
+int _isupper(int n)
+{
+if (n >= 'A' && n <= 'Z')
+	return (1);
+return (0);
+}
