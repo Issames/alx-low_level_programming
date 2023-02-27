137a18b7b00f3622054612dfbd22863988e81c1a
Issames <lamchabragissame.112@gmail.com>
2023-02-27 15:25:56 -0800

    P

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,14 +6,13 @@
  * @str: string
  * Return: void
  */
-void puts_half(char *str) 
+void puts_half(char *str)
 {
 int length = strlen(str);
 int start = length / 2;
-if (length % 2 == 1) 
+if (length % 2 == 1)
 {
 	start = (length - 1) / 2 + 1;
 }
 printf("%s\n", &str[start]);
 }
-
