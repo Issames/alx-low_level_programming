97396e65bba30fc53b4dae6982338f0da239ed17
Issames <lamchabragissame.112@gmail.com>
2023-02-27 22:57:40 -0800

    P

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,44 +1,37 @@
 #include "main.h"
 #include <limits.h>
 /**
- * _atoi : function
+ * _atoi: function that generate random valid passwords.
  * @s: pointer
  *
  * Return: Always
  */
-int _atoi(char *s) 
+int _atoi(char *s)
 {
 int result = 0;
 int sign = 1;
 int i = 0;
-
-while (s[i] == ' ')
+while (s[i] == ' ' || s[i] == '-' || s[i] == '+')
 {
-	i++;
-}
-if (s[i] == '-')
-{
-	sign = -1;
-	i++;
-}
-else if (s[i] == '+')
-{
+	if (s[i] == '-')
+	{
+		sign = -sign;
+	}
 	i++;
 }
 while (s[i] >= '0' && s[i] <= '9')
 {
 	if (result > INT_MAX / 10 || (result == INT_MAX / 10 && (s[i] - '0') > INT_MAX % 10))
 	{
-        	if (sign == 1)
+		if (sign == 1)
 		{
-                	return INT_MAX;
-            	} 
-		else 
+			return INT_MAX;
+            	}
+		else
 		{
-                	return INT_MIN;
+			return INT_MIN;
             	}
         }
-
 result = result * 10 + (s[i] - '0');
 i++;
 }
