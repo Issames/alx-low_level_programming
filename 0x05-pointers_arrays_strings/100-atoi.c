5a9c7fe057c4274e6641a627ff1fea274a536b92
Issames <lamchabragissame.112@gmail.com>
2023-02-27 23:06:18 -0800

    P

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -17,22 +17,29 @@ while (s[i] == ' ' || s[i] == '-' || s[i] == '+')
 	{
 		sign = -sign;
 	}
-	i++;
+i++;
 }
-while (s[i] >= '0' && s[i] <= '9')
+while (s[i] != '\0')
 {
-	if (result > INT_MAX / 10 || (result == INT_MAX / 10 && (s[i] - '0') > INT_MAX % 10))
+	if (s[i] >= '0' && s[i] <= '9')
 	{
-		if (sign == 1)
+		if (result > INT_MAX / 10 || (result == INT_MAX / 10 && (s[i] - '0') > INT_MAX % 10))
 		{
-			return INT_MAX;
-            	}
-		else
-		{
-			return INT_MIN;
-            	}
-        }
-result = result * 10 + (s[i] - '0');
+			if (sign == 1)
+			{
+				return INT_MAX;
+            		}
+			else
+			{
+				return INT_MIN;
+            		}
+        	}
+	result = result * 10 +(s[i] - '0');
+	}
+	else
+	{
+		break;
+	}
 i++;
 }
 return result * sign;
