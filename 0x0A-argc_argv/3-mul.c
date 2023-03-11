6847c23642ac4924498232687b3937f31a66ee73
Issames <lamchabragissame.112@gmail.com>
2023-03-11 04:56:30 -0800

    P

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,14 +9,14 @@
  */
 int main(int argc, char *argv[])
 {
-if (agrc <= 2 || argc > 3)
+if (argc <= 2 || argc > 3)
 {
 	printf("Error\n");
 	return (1);
 }
 else
 {
-	printf("%d\n", atoi(argv[i]) * atoi(argv[2]));
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
 }
