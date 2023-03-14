1e88bbd71228e0146f53895fd95ab35f1d8fdd02
Issames <lamchabragissame.112@gmail.com>
2023-03-14 13:35:52 -0700

    P

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - minimum number of coin 
+ * @cents: variable
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+int num_coins = 0;
+int cents;
+if (argc != 2)
+{
+	printf("Error\n");
+	return 1;
+}
+cents = atoi(argv[1]);
+if (cents < 0)
+{
+	printf("0\n");
+	return 0;
+}
+num_coins += cents / 25;
+cents %= 25;
+num_coins += cents / 10;
+cents %= 10;
+num_coins += cents / 5;
+cents %= 5;
+num_coins += cents / 2;
+cents %= 2;
+num_coins += cents;
+printf("%d\n", num_coins);
+return (0);
+}
