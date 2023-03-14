e821ad10e01256b4a0deefcccd3eb2b699564e6f
Issames <lamchabragissame.112@gmail.com>
2023-03-14 13:15:32 -0700

    P

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,12 +11,12 @@ int **alloc_grid(int width, int height)
 int **grid, i;
 if (width <= 0 || height <= 0)
 	return (NULL);
-grid = (int **) malloc(height *sizeof(int *));
+grid = (int **) malloc(height * sizeof(int *));
 if (grid == NULL)
 	return (NULL);
 for (i = 0; i < height; i++)
 {
-	grid[i] = malloc(width *sizeof(int));
+	grid[i] = malloc(width * sizeof(int));
 	if (grid[i] == NULL)
 	{
 		while (i >= 0)
