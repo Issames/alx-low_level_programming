ab0fdb2693fd3b7e7eac8366c4e60954ab8b9fd1
Issames <lamchabragissame.112@gmail.com>
2023-03-22 19:25:33 -0700

    P

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,30 +1,22 @@
 #include "3-calc.h"
-#include <stdio.h>
 #include <stdlib.h>
 /**
- * get_op_func - finds operation function
- * @s: operator (+, -, *, /, %)
- * Return: function pointer
+ * get_op_func - Selects the correct function to perform the operation asked by the user.
+ * @s: The operator passed as argument.
+ * Return: A pointer to the function corresponding to the operator given as a parameter.
  */
 int (*get_op_func(char *s))(int, int)
 {
-int i;
 op_t ops[] = {
 	{"+", op_add},
 	{"-", op_sub},
 	{"*", op_mul},
 	{"/", op_div},
 	{"%", op_mod},
-	{NULL, NULL}
+	{NULL, NULL},
 };
-i = 0;
-while (ops[i].op != NULL)
-{
-	if (*ops[i].op == *s && s[1] == '\0')
-	{
-		return (ops[i].f);
-	}
+int i = 0;
+while (ops[i].op != NULL && *(ops[i].op) != *s)
 	i++;
-}
-return (NULL);
+return (ops[i].f);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,30 +1,35 @@
-#include "3-calc.h"
+#include "function_pointers.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include "3-calc.h"
 /**
- * main - performs simple operations (calculator)
- * @argc: number of arguments
- * @argv: array of arguments
- * Return: 0 (Always)
+ * main - Prints the result of simple operations.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ * Return: Always 0.
  */
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-int a, b, result;
-int (*op)(int, int);
+int num1, num2;
+char *op;
 if (argc != 4)
 {
 	printf("Error\n");
 	exit(98);
 }
-op = get_op_func(argv[2]);
-if (op == NULL)
+num1 = atoi(argv[1]);
+op = argv[2];
+num2 = atoi(argv[3]);
+if (get_op_func(op) == NULL || op[1] != '\0')
 {
 	printf("Error\n");
 	exit(99);
 }
-a = atoi(argv[1]);
-b = atoi(argv[3]);
-result = op(a, b);
-printf("%d\n", result);
+if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
+{
+	printf("Error\n");
+	exit(100);
+}
+printf("%d\n", get_op_func(op)(num1, num2));
 return (0);
 }
